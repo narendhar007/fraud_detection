04586a36d7878f7f9f2c1c7ba48337976e2fcc00
narendhar007 <narismailbox@gmail.com>
2025-01-08 14:42:56 +0000

    Added 3 models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print('This is a new file ml_model.py')
+
+def model1():
+    print('This is a model1 function')
+
+def model2():
+    print('This is a model2 function')
+
+def model3():
+    print('This is a model3 function')
+    
