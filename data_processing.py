fa6c1e21665d4441807a32b36cba3855723157f6
narendhar007 <narismailbox@gmail.com>
2025-01-08 10:32:51 +0000

    Added sum function to data_proccessing.py

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,14 @@
 print('This is a message from fraud detection repository using python print statement')
+
+print('Checking what happens if we commit without a commit message')
+
+#Add a sum function
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return a
+
+print('Added a sum function to the code')
